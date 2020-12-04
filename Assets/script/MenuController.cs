4ec83aa69102186dc0d8feea6ca301cce1c04f8f
YSoejima <gureiosugguto@gmail.com>
2020-12-04 15:39:18 +0900

    メニュー画面をPADで表示・非表示をできるようにした

--- Assets/script/MenuController.cs
+++ Assets/script/MenuController.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] Canvas menu;
     // Start is called before the first frame update
     void Start()
     {
-        
+        menu.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Menu"))
+        {
+            Debug.Log("Menu");
+            if (menu.gameObject.activeSelf == false)
+            {
+                menu.gameObject.SetActive(true);
+            }
+            else
+            {
+                menu.gameObject.SetActive(false);
+            }
+        }
     }
 
-    void ItemMenu()
-    {
-
-    }
-
-    void SkillMenu()
-    {
-
-    }
+    
 }
