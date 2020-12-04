e4b4413743282fa6962b041e861b6d24a7757d7f
YSoejima <gureiosugguto@gmail.com>
2020-12-04 17:34:19 +0900

    メニュー画面をPADでボタンを選択できるようにした

---
+++ Assets/script/ButtonEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnSelected()
+    {
+        //　イベントシステムのフォーカスが他のゲームオブジェクトにある時このゲームオブジェクトにフォーカス
+        if (EventSystem.current.currentSelectedGameObject != gameObject)
+        {
+            EventSystem.current.SetSelectedGameObject(gameObject);
+        }
+    }
+}
--- Assets/script/MenuController.cs
+++ Assets/script/MenuController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MenuController : MonoBehaviour
 {
     [SerializeField] Canvas menu;
+    [SerializeField] GameObject[] windowLists;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,17 +18,26 @@ public class MenuController : MonoBehaviour
     {
         if (Input.GetButtonDown("Menu"))
         {
-            Debug.Log("Menu");
-            if (menu.gameObject.activeSelf == false)
-            {
-                menu.gameObject.SetActive(true);
-            }
-            else
-            {
-                menu.gameObject.SetActive(false);
-            }
+            menu.gameObject.SetActive(!menu.gameObject.activeSelf);
+            ChangeWindow(windowLists[0]);
         }
     }
 
-    
+    public void ChangeWindow(GameObject window)
+    {
+        foreach (var item in windowLists)
+        {
+            if (item == window)
+            {
+                item.SetActive(true);
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+            else
+            {
+                item.SetActive(false);
+            }
+            //　それぞれのウインドウのMenuAreaの最初の子要素をアクティブな状態にする
+            EventSystem.current.SetSelectedGameObject(window.transform.Find("MenuArea").GetChild(0).gameObject);
+        }
+    }
 }
