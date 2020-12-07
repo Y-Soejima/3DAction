e524e76f781b6c223e817bc9432ae9dc4552471d
YSoejima <gureiosugguto@gmail.com>
2020-12-07 18:20:54 +0900

    HpHealを追加

---
+++ Assets/script/Item/HpHeal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpHeal : ItemController
+{
+    public void Heal()
+    {
+        CharactorControllerRb.FindObjectOfType<CharactorControllerRb>().HpHeal();
+        base.Use();
+    }
+}
