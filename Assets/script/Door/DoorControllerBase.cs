8df8cf170ba55089055186026e91fe47d94c386c
YSoejima <gureiosugguto@gmail.com>
2020-12-09 06:26:03 +0900

    DoorControllerを変更

---
+++ Assets/script/Door/DoorControllerBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControllerBase: MonoBehaviour
+{
+    Animator door;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        door = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    public void Open()
+    {
+        if (door)
+        door.Play("Open");
+    }
+}
