dd064f6fc2ae12d6ed7210e84e7293d5677103a2
YSoejima <gureiosugguto@gmail.com>
2020-12-11 17:22:59 +0900

    ItemButtonを追加

---
+++ Assets/script/Item/ItemButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemButton : MonoBehaviour
+{
+    [SerializeField] Text information;
+    [SerializeField] ItemController item;
+    [SerializeField] int itemNumber;
+    ItemController itemInventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        item = GetComponent<ItemController>();
+        itemInventory = PlayerStatus.FindObjectOfType<PlayerStatus>().itemInventory[itemNumber];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSelected()
+    {
+        information.text = item.itemInformation;
+    }
+
+    public void DeSelected()
+    {
+        information.text = "";
+    }
+
+    public void ItemUse()
+    {
+        if (itemInventory)
+        {
+            itemInventory.Use();
+            PlayerStatus.FindObjectOfType<PlayerStatus>().itemCounter[itemNumber] = itemInventory.itemCount;
+        }
+    }
+}
