25696d556329c1adc7ab3bcfbe1c9e9728078bb8
YSoejima <gureiosugguto@gmail.com>
2021-02-27 02:11:43 +0900

    boss前の扉をプレイヤーが近づくと開くように変更

---
+++ Assets/script/Door/PlayerSeachDoor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSeachDoor : MonoBehaviour
+{
+    Animator door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        door = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == ("Player"))
+        {
+            door.SetBool("isPlayer", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        door.SetBool("isPlayer", false);
+    }
+}
